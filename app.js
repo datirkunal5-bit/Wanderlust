522dfd4b3a2e83a5f93cce04e7c85247c7f70824
Kunal Datir <datirkunal5@gmail.com>
2026-05-19 20:36:50 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.set("view engine", "ejs");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+app.get("/about",(req,res)=>{
+  res.render("about");
+});
+
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
