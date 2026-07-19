f1b04bebc52b35e052b396910c0e97f72a76d646
Kunal Datir <datirkunal5@gmail.com>
2026-07-19 19:49:16 +0530

    Add static file serving and basic CSS styling

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const ejsMate = require("ejs-mate");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
 // View Engine
 app.set("view engine", "ejs");
